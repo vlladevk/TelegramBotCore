9fa18093bfb92c987590fc8b3983f6cb5ca16b2a
vllad <vlladevk04@gmail.com>
2025-04-21 19:58:48 +0200

    test: add unit tests for DefaultCommandExtractor

---
+++ src/test/java/org/pl/pcz/yevkov/botcore/application/command/parser/DefaultCommandExtractorTest.java
@@ -0,0 +1,33 @@
+package org.pl.pcz.yevkov.botcore.application.command.parser;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultCommandExtractorTest {
+    private final DefaultCommandExtractor extractor = new DefaultCommandExtractor();
+
+    private static Stream<Arguments> provideTestArguments() {
+        return Stream.of(
+                Arguments.of("command arg1 arg2", "command"),
+                Arguments.of("command", "command"),
+                Arguments.of("   command   ", "command"),
+                Arguments.of("command@bot arg1", "command"),
+                Arguments.of("command@bot", "command"),
+                Arguments.of("command@bot@extra arg1", "command"),
+                Arguments.of("\tcommand\targ1", "command"),
+                Arguments.of("   command@bot    arg1 arg2", "command")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestArguments")
+    void extract_testCommandExtraction_returnCommand(String input, String expectedResult) {
+        String result = extractor.extract(input);
+        assertEquals(expectedResult, result);
+    }
+}
